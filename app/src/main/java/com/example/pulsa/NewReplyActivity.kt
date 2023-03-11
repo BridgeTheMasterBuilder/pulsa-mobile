566c3fb8da06d555e30f25fac568ea23b8776039
Master Builder <gylfi@stebbi.is>
2023-03-11 16:34:20 +0000

    Aðeins kominn áleiðis með audio recording

--- app/src/main/java/com/example/pulsa/NewReplyActivity.kt
+++ app/src/main/java/com/example/pulsa/NewReplyActivity.kt
@@ -1,16 +1,84 @@
 package com.example.pulsa
 
+import android.Manifest
 import android.app.Activity
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.example.pulsa.databinding.ActivityNewReplyBinding
+import java.io.IOException
 import java.time.LocalDateTime
+
+private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
 
 class NewReplyActivity : BaseLayoutActivity() {
     private lateinit var binding: ActivityNewReplyBinding
+    private lateinit var mediaRecorder: MediaRecorder
+    private lateinit var mediaPlayer: MediaPlayer
+    private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
+    private lateinit var fileName: String
+    private var recording = false
+    private var playing = false
+    private var permissionGranted = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNewReplyBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        fileName = "${externalCacheDir?.absolutePath}/temp.3gp"
+
+        binding.recordbutton.setOnClickListener {
+
+            if (recording) {
+                recording = !recording
+                mediaRecorder.stop()
+                mediaRecorder.release()
+
+                mediaPlayer = MediaPlayer().apply {
+                    try {
+                        setDataSource(fileName)
+                        prepare()
+                    } catch (e: IOException) {
+                        // todo
+                        System.err.println("oops")
+                    }
+                }
+            } else if (permissionGranted) {
+                recording = !recording
+                mediaRecorder.start()
+            } else
+                ActivityCompat.requestPermissions(
+                    this,
+                    permissions,
+                    REQUEST_RECORD_AUDIO_PERMISSION
+                )
+        }
+
+        binding.playbutton.setOnClickListener {
+            Toast.makeText(this, "Playing", Toast.LENGTH_SHORT).show()
+            if (recording) {
+                mediaRecorder.stop()
+                mediaRecorder.release()
+
+                mediaPlayer = MediaPlayer().apply {
+                    try {
+                        setDataSource(fileName)
+                        prepare()
+                    } catch (e: IOException) {
+                        // todo
+                        System.err.println("oops")
+                    }
+                }
+            } else if (playing)
+                mediaPlayer.stop()
+
+            playing = !playing
+
+            mediaPlayer.start()
+        }
 
         binding.postbutton.setOnClickListener {
             val text = binding.newreplytext.text.toString()
@@ -57,5 +125,48 @@ class NewReplyActivity : BaseLayoutActivity() {
             setResult(Activity.RESULT_OK, intent)
             finish()
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) onPermissionsGranted()
+//        permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+//            grantResults[0] == PackageManager.PERMISSION_GRANTED
+//        } else {
+//            false
+//        }
+//        if (!permissionToRecordAccepted) finish()
+    }
+
+    private fun onPermissionsGranted() {
+        Toast.makeText(
+            this,
+            "success!",
+            Toast.LENGTH_SHORT
+        ).show()
+
+        mediaRecorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setOutputFile(fileName)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+                // todo
+                System.err.println("oops")
+            }
+
+            recording = true
+
+            start()
+        }
+
+
     }
 }
