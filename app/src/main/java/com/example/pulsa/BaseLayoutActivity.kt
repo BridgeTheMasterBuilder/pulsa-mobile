4e42af868b1188576e3bf3c0cd680c7574afe082
hexvessel <dho1@hi.is>
2023-03-12 17:49:13 +0000

    User management page fragment fix

--- app/src/main/java/com/example/pulsa/BaseLayoutActivity.kt
+++ app/src/main/java/com/example/pulsa/BaseLayoutActivity.kt
@@ -87,6 +87,10 @@ open class BaseLayoutActivity : AppCompatActivity() {
                     val i = Intent(this, RegisterActivity::class.java)
                     startActivity(i)
                 }
+                R.id.editacc -> {
+                    val i = Intent(this, UserActivity::class.java)
+                    startActivity(i)
+                }
             }
             true
         }
