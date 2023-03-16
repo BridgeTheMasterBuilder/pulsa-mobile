43242daecc6d67485b306ae4e8a23111ca8850fe
Selph <arni@hugsnilld.is>
2023-03-16 18:16:38 +0000

    Get posts start

--- app/src/main/java/com/example/pulsa/networking/NetworkManager.kt
+++ app/src/main/java/com/example/pulsa/networking/NetworkManager.kt
@@ -1,8 +1,23 @@
 package com.example.pulsa.networking
 
 import android.content.Context
+import okhttp3.Call
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 class NetworkManager private constructor(context: Context) {
     private val URL = "localhost:8080"
     private val mContext: Context = context.applicationContext
+    private val client: OkHttpClient = OkHttpClient()
+
+    private fun getPosts() {
+        val request: Request = Request.Builder()
+            .url(URL)
+            .build()
+
+        val call: Call = client.newCall(request)
+        call.enqueue(Callback)
+    }
+
+
 }
