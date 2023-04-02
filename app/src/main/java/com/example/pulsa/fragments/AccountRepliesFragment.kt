25b4c2bf515017e295820c4d4954d8c24abc599f
hexvessel <dho1@hi.is>
2023-04-02 20:53:47 +0000

    Allskonar user page breytingar part deuce

--- app/src/main/java/com/example/pulsa/fragments/AccountRepliesFragment.kt
+++ app/src/main/java/com/example/pulsa/fragments/AccountRepliesFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.pulsa.R
-import com.example.pulsa.adapters.GenericRecyclerAdapter
+import com.example.pulsa.adapters.ReplyRecyclerAdapter
 import com.example.pulsa.databinding.FragmentAccountRepliesBinding
 import com.example.pulsa.objects.Reply
 import com.example.pulsa.services.PostService
@@ -14,8 +14,8 @@ import com.example.pulsa.services.PostService
 class AccountRepliesFragment : Fragment() {
 
     private lateinit var binding: FragmentAccountRepliesBinding
-    private lateinit var adapter: GenericRecyclerAdapter<Reply>
-    private lateinit var posts: MutableList<Reply>
+    private lateinit var adapter: ReplyRecyclerAdapter<Reply>
+    private lateinit var replies: MutableList<Reply>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,8 +23,8 @@ class AccountRepliesFragment : Fragment() {
     ): View? {
         binding = FragmentAccountRepliesBinding.inflate(inflater, container, false)
         val view = binding.root
-        posts = PostService().replies
-        adapter = GenericRecyclerAdapter(posts, ::adapterOnClick, R.layout.post_item)
+        replies = PostService().replies
+        adapter = ReplyRecyclerAdapter(replies, ::adapterOnClick, R.layout.reply)
         binding.recyclerView.adapter = adapter
         return view
     }
